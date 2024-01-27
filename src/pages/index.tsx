4463434514a331a7bd472a1e43a51db0df8785a1
Анударь Алтангэрэл <HP230043@G303-CC0156.local>
2024-01-27 15:15:12 +0800

    home page edited

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter()
   return (
     <>
     hello world
+    <button onClick={() => router.push("/login")}>login page</button>
     </>
   );
 }
